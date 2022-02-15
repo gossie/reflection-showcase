b7a004aa287c9e019d450c9066c4cb058ae92c2a
André Schreck <moin@andreschreck.de>
2022-02-15 20:35:11 +0100

    comments

--- src/test/java/com/github/gossie/reflection/ProductTest.java
+++ src/test/java/com/github/gossie/reflection/ProductTest.java
@@ -11,17 +11,27 @@ class ProductTest {
 
     @Test
     void reflectionTest() throws Exception {
+        // Ich hole mit die Klasse Product als Class Object
         Class<Product> productClass = Product.class;
+        // Ich hole mir den Default Konstruktor als Constructor Objekt
         Constructor<Product> defaultConstructor = productClass.getConstructor();
+        // Ich rufe den Default Konstruktor auf und erzeuge mit eine neue Instanz
         Object theProductAsObject = defaultConstructor.newInstance();
 
-        assertInstanceOf(Product.class, theProductAsObject); // jetzt weiß ich, dass theProductAsObject ein Product ist
+        // Jetzt weiß ich, dass theProductAsObject ein Product ist
+        assertInstanceOf(Product.class, theProductAsObject);
 
+        // Ich hole mir von der Klasse die Methode setName als Method Objekt
         Method setName = productClass.getMethod("setName", String.class);
+        // Ich rufe die Methode setName an der Instanz theProductAsObject auf
         setName.invoke(theProductAsObject, "Mein Produkt");
 
+        // Ich hole mir von der Klasse die Methode getId als Method Objekt
         Method getId = productClass.getMethod("getId");
+        // Ich rufe die Methode getId an der Instanz theProductAsObject auf und merke mit den Rückgabewert
         String productId = (String) getId.invoke(theProductAsObject);
+
+        // Alles überprüfen
 
         Product theProduct = (Product) theProductAsObject;
 
